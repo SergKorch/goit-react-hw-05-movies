99f062dcd887cef8e813967ccebfad60fedcbeaa
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-06-07 22:02:39 +0300

    update

--- src/components/InfoFilm/InfoFilm.jsx
+++ src/components/InfoFilm/InfoFilm.jsx
@@ -1,10 +1,24 @@
+import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import movieId from 'services/movieId/';
 const InfoFilm = () => {
-    
-    return (
-        <div>
-          <h1>Info film</h1>  
-        </div>
-    )}
-    
-    
-    export default InfoFilm
+  const { filmId } = useParams();
+  const [ film, setFilm ] = useState(null);
+  useEffect(() => {
+    movieId(filmId)
+      .then(setFilm)
+      .catch(err => console.log(err));
+  }, [filmId, setFilm]);
+  console.log(film);
+  return (
+    <div>
+    <img src="" alt="" />
+      <h1>{`Film ${filmId}`}</h1>
+      <Link to={`/films/${filmId}/cast`}>Casts</Link>
+      <Link to={`/films/${filmId}/reviews`}>Reviews</Link>
+    </div>
+    // {film && }
+  );
+};
+
+export default InfoFilm;
