92c5a368f37042bf4bf2fa025a14ad8ccba29f36
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-06-08 23:42:38 +0300

    to main

--- src/components/NotFind/NotFind.jsx
+++ src/components/NotFind/NotFind.jsx
@@ -1,7 +1,14 @@
+import { Link, useLocation } from 'react-router-dom';
+import s from './notFind.module.css'
+
 const NotFind = () => {
+  const location = useLocation();
   return (
     <div>
       <h1>Not find</h1>
+      <Link to="/" state={{ from: location }} className={s.notFind}>
+        Go to Main
+      </Link>
     </div>
   );
 };
