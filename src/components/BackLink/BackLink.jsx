3304191c3d6f791d8a98d658ad4120fa7093244a
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-06-10 15:04:41 +0300

    Update BackLink.jsx

--- src/components/BackLink/BackLink.jsx
+++ src/components/BackLink/BackLink.jsx
@@ -1,14 +1,19 @@
 import { useLocation } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
-
 import { Link } from 'react-router-dom';
-
 import s from './BackLink.module.css';
+import { useEffect, useState } from 'react';
 const BackLink = () => {
+  const [form, setForm] = useState(null);
   const location = useLocation();
+  useEffect(() => {
+    if (location.state) {
+      setForm(location.state.from);
+    }
+  }, [location]);
   return (
     <div className={s.link}>
-      <Link to={location.state?.from ?? '/'}>
+      <Link to={form ?? '/'}>
         <FaArrowLeft />
         Go back
       </Link>
